f747c25a0d3959747b99de5955e24b68c08bf523
maha lakshmi <mahalakshmi.kidombi@rubiconred.com>
2022-01-17 14:27:01 +0530

    forgot message

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ const messageTwo = document.querySelector('#message-2')
         const location = searchELement.value;
         messageOne.textContent = "loading ...."
         messageTwo.textContent = ""
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error;
